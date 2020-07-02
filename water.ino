857a0181ff7be194757b21977b009bad5cee8324
Caroline Dunn <thecarolinedunn@gmail.com>
2020-07-02 11:08:25 -0400

    Add files via upload

---
+++ water.ino
@@ -0,0 +1,18 @@
+int sensor_pin = A0; 
+int output_value ;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Reading From the Sensor ...");
+  delay(2000);
+  }
+
+void loop() {
+
+  output_value= analogRead(sensor_pin);
+  output_value = map(output_value,550,0,0,100);
+  Serial.print("Mositure : ");
+  Serial.print(output_value);
+  Serial.println("%");
+  delay(1000);
+  }
